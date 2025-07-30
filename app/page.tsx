48d21160f13baced047f36d564e6405a653d2b23
Shane Perry <shanegp127@gmail.com>
2025-07-30 18:38:56 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
+      <button>
+        Let's get started
+      </button>
+    </div>
+  )
+}
+
+export default Page
